d160b41fb121192cb4d359a7e0718a51eac4c874
34j <55338215+34j@users.noreply.github.com>
2023-10-07 12:04:44 +0900

    feat(id): add `refresh` parameter to `set_ids()`

--- src/cached_historical_data_fetcher/cache/id.py
+++ src/cached_historical_data_fetcher/cache/id.py
@@ -42,6 +42,7 @@ class IdCacheWithFixedChunk(
 
     add_interval_to_start_index: bool = False
     subtract_interval_from_end_index: bool = False
+    ids: Index[TIndex] | None
 
     @property
     def start_index(self) -> TIndex | None:  # type: ignore
@@ -51,12 +52,26 @@ class IdCacheWithFixedChunk(
     def end_index(self) -> TIndex | None:  # type: ignore
         return None
 
-    def set_ids(self, ids: Sequence[TIndex]) -> None:
-        self.ids = Index(ids)
+    def set_ids(self, ids: Sequence[TIndex], *, refresh: bool = False) -> None:
+        """Set ids to be fetched.
+
+        Parameters
+        ----------
+        ids : Sequence[TIndex]
+            New ids to be stored
+        refresh : bool, optional
+            If True, ids will be replaced by the given ids.
+            If False, ids will be added to the existing ids, by default False
+        """
+        if self.ids is None or refresh:
+            self.ids = Index(ids)
+        self.ids = self.ids.union(Index(ids))
 
     def new_indices(
         self, start: Any, end: Any, old_indices: Sequence[TIndex]
     ) -> Sequence[TIndex]:
+        if self.ids is None:
+            raise RuntimeError("ids is not set, call `set_ids()` first")
         return self.ids.difference(Index(old_indices)).to_list()
 
     def to_update(self, end: TIndex, *args: Any, **kwargs: Any) -> bool:
